f7b66e7255bfcd9be4bb4cbf68e42cfa912a6e38
Amirhossein <khadivi.ah@yahoo.com>
2022-09-07 01:21:10 +0430

    qe

---
+++ quadratic equation.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+	float x1,x2;
+	int a,b,c,delta;
+	cin >> a >> b>> c;
+	if (a==0)
+	{
+		cout << "This equation has no roots" << endl;
+	}
+	return 0;
+}
